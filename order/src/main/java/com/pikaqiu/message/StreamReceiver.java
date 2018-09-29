ab51e9353fadd063cb811a2ed4ee43ecefed8c3d
lixinyi <xinyi.li2@pactera.com>
2018-09-29 17:11:30 +0800

    第一次提交

--- order/src/main/java/com/pikaqiu/message/StreamReceiver.java
+++ order/src/main/java/com/pikaqiu/message/StreamReceiver.java
@@ -27,7 +27,7 @@ public class StreamReceiver {
 */
     /**直接接受对象 它会自动转换
      * @param resultvo
-  /*   *//*
+  /*
     @StreamListener(StreamClient.input)
     public void process(ResultVO resultvo) {
         log.info("resultVo:{}", resultvo);
